86f6db42bf586e1c89fd0d27a90a3c5b6bb6ec35
t1ger <1024472394@qq.com>
2021-04-27 14:23:51 +0800

    .

--- src/main/java/com/tiger/paper/two/Two_SSA_Multimodal.java
+++ src/main/java/com/tiger/paper/two/Two_SSA_Multimodal.java
@@ -13,13 +13,13 @@ public class Two_SSA_Multimodal {
     /**
      * 控制小数点后几位的精度
      */
-    private static final int PRECISION = 15;
+    private static final int PRECISION = 30;
     /**
      * 维度
      */
-    private static final int DIMENSION = 3;
-    private static final int UPPER_BOUND = 10;
-    private static final int LOWER_BOUND = -10;
+    private static final int DIMENSION = 30;
+    private static final int UPPER_BOUND = 100;
+    private static final int LOWER_BOUND = -100;
     /**
      * 种群大小
      */
@@ -136,14 +136,14 @@ public class Two_SSA_Multimodal {
                 for (int j = 0; j < DIMENSION; j++) {
                     do {
                         sparrowJDimension = sparrowIndex.get(j) * Math.exp(-(i / (iterations * Math.random())));
-                    } while (sparrowJDimension < 0 || sparrowJDimension > 1);
+                    } while (sparrowJDimension < LOWER_BOUND || sparrowJDimension > UPPER_BOUND);
                     sparrowIndex.set(j, packagingAccuracy(sparrowJDimension));
                 }
             } else {
                 for (int j = 0; j < DIMENSION; j++) {
                     do {
                         sparrowJDimension = sparrowIndex.get(j) + randomNormalDistribution();
-                    } while (sparrowJDimension < 0 || sparrowJDimension > 1);
+                    } while (sparrowJDimension < LOWER_BOUND || sparrowJDimension > UPPER_BOUND);
                     sparrowIndex.set(j, packagingAccuracy(sparrowJDimension));
                 }
             }
@@ -203,12 +203,124 @@ public class Two_SSA_Multimodal {
         }
     }
 
+    public double calculate() {
+        for (int i = 1; i <= iterations; i++) {
+            r2 = Math.random();
+            //更新发现者坐标
+            updateProducerPoint();
+//            System.out.println("更新发现者坐标");
+            //更新最优坐标1
+            rankAndFindLocation();
+//            System.out.println("更新最优坐标1");
+            //更新追随者坐标
+            updateScroungerPoint();
+//            System.out.println("更新追随者坐标");
+            //更新最优坐标2
+            rankAndFindLocation();
+//            System.out.println("更新最优坐标2");
+            //更新预警者坐标
+            updateSDPoint();
+//            System.out.println("更新预警者坐标");
+            //更新最优坐标3
+            rankAndFindLocation();
+//            System.out.println("更新最优坐标3");
+        }
+        return updateValue.get("fg");
+    }
+
+    private void updateSDPoint() {
+        List<Double> sparrowIndex;
+        double f;
+        double fg = updateValue.get("fg");
+        double fw = updateValue.get("fw");
+        List<Double> globalMax = updateMap.get("globalMax");
+        List<Double> globalMin = updateMap.get("globalMin");
+        double sparrowJDimension = 0;
+        Set<Integer> hashSet = new HashSet<>();
+        int idx;
+        for (int i = 0; i < SD; i++) {
+            do {
+                idx = random.nextInt(speciesNum);
+            } while (!hashSet.add(idx));
+            sparrowIndex = coordinatePoints.get(idx);
+            f = fSphere(sparrowIndex);
+
+            if (f > fg) {
+                for (int j = 0; j < DIMENSION; j++) {
+                    do {
+                        sparrowJDimension = globalMax.get(j) + randomNormalDistribution() * Math.abs(globalMax.get(j) - sparrowIndex.get(j));
+                    } while (sparrowJDimension < LOWER_BOUND || sparrowJDimension > UPPER_BOUND);
+                    sparrowIndex.set(j, packagingAccuracy(sparrowJDimension));
+                }
+            } else if (Math.abs(f - fg) <= 1e-14) {
+                for (int j = 0; j < DIMENSION; j++) {
+                    double abs = Math.abs(sparrowIndex.get(j) - globalMin.get(j));
+                    double v = fw - f + 1e-14;
+                    double v1 = abs / v;
+//                    while (Math.abs(v1) > 1) {
+//                        v1 /= 10;
+//                    }
+                    int cnt = 1;
+                    do {
+                        if (cnt > 50) {
+                            break;
+                        }
+                        sparrowJDimension = sparrowIndex.get(j) + (Math.random() * 2 - 1) * v1;
+                        cnt++;
+                    } while (sparrowJDimension < LOWER_BOUND || sparrowJDimension > UPPER_BOUND);
+                    if (cnt > 50) {
+                        sparrowJDimension = updateMap.get("globalMax").get(j);
+                    }
+                    sparrowIndex.set(j, packagingAccuracy(sparrowJDimension));
+                }
+            }
+        }
+    }
+
+    private void updateScroungerPoint() {
+        List<Double> globalMin = updateMap.get("globalMin");
+        List<Double> sparrowIndex;
+        double sparrowJDimension;
+        List<Double> pdMax = updateMap.get("pdMax");
+        for (int i = 0; i < speciesNum - PD; i++) {
+            sparrowIndex = scPoints.get(i);
+
+            if (i > speciesNumDivideTwo) {
+                for (int j = 0; j < DIMENSION; j++) {
+                    do {
+                        sparrowJDimension = randomNormalDistribution() * Math.exp((globalMin.get(j) - sparrowIndex.get(j)) / Math.pow(i, 2));
+                    } while (sparrowJDimension < LOWER_BOUND || sparrowJDimension > UPPER_BOUND);
+                    sparrowIndex.set(j, packagingAccuracy(sparrowJDimension));
+                }
+            } else {
+                for (int j = 0; j < DIMENSION; j++) {
+                    do {
+                        sparrowJDimension = pdMax.get(j) + Math.abs(sparrowIndex.get(j) - pdMax.get(j)) * (Math.random() > 0.5 ? 1 : -1);
+                    } while (sparrowJDimension < LOWER_BOUND || sparrowJDimension > UPPER_BOUND);
+                    sparrowIndex.set(j, packagingAccuracy(sparrowJDimension));
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        new Two_SSA_Multimodal(10, 500, 0.8d, 0.1d, 0.8d);
+        double res[] = new double[30];
+        double sum = 0.0d;
+        for (int i = 0; i < 30; i++) {
+            res[i] = new Two_SSA_Multimodal(100, 1000, 0.8d, 0.1d, 0.8d).calculate();
+            sum += res[i];
+        }
+        double mean = sum / res.length;
+        System.out.println(BigDecimal.valueOf(mean).setScale(PRECISION, RoundingMode.HALF_UP).doubleValue());
+        sum = 0.0d;
+        for (int i = 0; i < res.length; i++) {
+            sum += Math.pow(res[i] - mean, 2);
+        }
+        System.out.println(BigDecimal.valueOf(Math.sqrt(sum / res.length)).setScale(PRECISION, RoundingMode.HALF_UP).doubleValue());
     }
 
     /**
-     * Σ X^2 -- sphere  单峰函数
+     * Σ X^2 -- sphere  单峰函数   -100 100   最优解 0
      *
      * @param xSet 多维 x 集合
      * @return
@@ -222,21 +334,97 @@ public class Two_SSA_Multimodal {
     }
 
     /**
-     * Griewank 函数 多峰
+     * Rosenbrock 函数 单峰  -30 30  最优解 0
+     *
+     * @param xSet 多维 x 集合
+     * @return
+     */
+    private double fRosenbrock(List<Double> xSet) {
+        double sum = 0;
+        for (int i = 0; i < DIMENSION - 1; i++) {
+            sum += (100 * Math.pow(xSet.get(i + 1) - Math.pow(xSet.get(i), 2), 2) + Math.pow(1 - xSet.get(i), 2));
+        }
+        return packagingAccuracy(sum);
+    }
+
+    /**
+     * fSchwefel_2_22函数 单峰 -10 10 最优解 0
      *
      * @param xSet
      * @return
      */
-    private double fGriewank(List<Double> xSet) {
+    private double fSchwefel_2_22(List<Double> xSet) {
         double part1 = 0;
         for (Double x : xSet) {
-            part1 += Math.pow(x, 2) / 4000;
+            part1 += Math.abs(x);
         }
         double part2 = 1;
-        for (int i = 0; i < xSet.size(); i++) {
-            part2 *= Math.cos(xSet.get(i) / Math.sqrt(i + 1));
+        for (Double x : xSet) {
+            part2 *= Math.abs(x);
         }
-        return packagingAccuracy(part1 - part2 + 1);
+        return packagingAccuracy(part1 + part2);
+    }
+
+    /**
+     * Griewank函数 多峰  -600 600  最优解 0
+     *
+     * @param xSet
+     * @return
+     */
+//    private double fGriewank(List<Double> xSet) {
+//        double part1 = 0;
+//        for (Double x : xSet) {
+//            part1 += Math.pow(x, 2) / 4000;
+//        }
+//        double part2 = 1;
+//        for (int i = 0; i < xSet.size(); i++) {
+//            part2 *= Math.cos(xSet.get(i) / Math.sqrt(i + 1));
+//        }
+//        return packagingAccuracy(part1 - part2 + 1);
+//    }
+
+    /**
+     * Rastrigin函数 多峰  -5.12 5.12  最优解 0
+     *
+     * @param xSet
+     * @return
+     */
+    private double fRastrigin(List<Double> xSet) {
+        double sum = 0;
+        for (Double x : xSet) {
+            sum += (Math.pow(x, 2) - 10 * Math.cos(2 * Math.PI * 2) + 10);
+        }
+        return packagingAccuracy(sum);
+    }
+
+    /**
+     * Alpine函数 多峰  -10 10  最优解 0
+     *
+     * @param xSet
+     * @return
+     */
+    private double fAlpine(List<Double> xSet) {
+        double sum = 0;
+        for (Double x : xSet) {
+            sum += Math.abs(x * Math.sin(x) + 0.1 * x);
+        }
+        return packagingAccuracy(sum);
+    }
+
+    /**
+     * Acley函数 多峰  -32 32  最优解 0
+     *
+     * @param xSet
+     * @return
+     */
+    private double fAcley(List<Double> xSet) {
+        double part1 = 0;
+        double part2 = 0;
+        for (Double x : xSet) {
+            part1 += Math.pow(x, 2);
+            part2 += Math.cos(2 * Math.PI * x);
+        }
+        return packagingAccuracy(20 + Math.E - 20 * Math.exp(-0.2 * Math.sqrt(part1 / DIMENSION)) - Math.exp(part2 / DIMENSION));
     }
 
     /**
